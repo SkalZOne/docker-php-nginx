f84a2ec78acc6d725173d0e001b09edab0f26926
SkalZOne <dima.tysyahnyy@mail.ru>
2025-03-23 20:49:40 +0500

    added start.sh statistics output

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+WHITE='\033[0;37m'
+BOLD_WHITE='\033[1;37m'
+BOLD_GREEN='\033[1;32m'
+BOLD_YELLOW='\033[1;33m'
+
+DOMAIN_IP_ADDRESS="$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')"
+
+# Запускаем контейнеры
 cd docker
 docker compose up -d --build
+
+## Адреса веб-интерфейсов
+echo -e "\nАдрес WEB-Интерфейса: ${WHITE}http://${DOMAIN_IP_ADDRESS}:8080/"
