59660487ac2a5d452df5fdb41b325585c50bca3c
SkalZOne <dima.tysyahnyy@mail.ru>
2025-03-23 23:34:15 +0500

    added new code for redis testings in index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "server started";
+$client = new \Predis\Client(
+    [
+        ["host" => "83.222.27.169", "port" => 6379],
+        ["host" => "217.114.10.239", "port" => 6379]
+    ],
+    ['cluster' => 'redis']
+);
+
+$client->set('foo', 'bar');
+
+$value = $client->get('foo');
+
+var_dump($value);
