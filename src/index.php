14c2dae17ff1213705f8430099f810cfff5a21fb
SkalZOne <dima.tysyahnyy@mail.ru>
2025-04-19 19:35:56 +0500

    correcting default src folder

--- src/index.php
+++ src/index.php
@@ -1,15 +1,3 @@
 <?php
 
-$client = new \Predis\Client(
-    [
-        ["host" => "83.222.27.169", "port" => 6379],
-        ["host" => "217.114.10.239", "port" => 6379]
-    ],
-    ['cluster' => 'redis']
-);
-
-$client->set('foo', 'bar');
-
-$value = $client->get('foo');
-
-var_dump($value);
+var_dump('Запущено');
