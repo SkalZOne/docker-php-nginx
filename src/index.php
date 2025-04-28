cee4ab000d418e45834966d5aa8cc52eebc5cbc1
Дмитрий <132896915+SkalZOne@users.noreply.github.com>
2025-04-29 00:41:20 +0500

    Newfilessova (#1)
    * refactoring composer install in containers
    
    * correcting www.conf
    
    * correcting antivirus installation
    
    * correcting php auto include option
    
    * added vendor folder in .gitignore
    
    * deleted autoload
    
    * correcting default src folder
    
    * installation project
    
    * deleted auto vendor string
    
    * added actual files from server

--- src/index.php
+++ src/index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="auth.php" method="POST">
-        <input name="email_phone" type="text" placeholder="Почта или телефон">
-        <input name="password" type="password" placeholder="Пароль">
-        <button type="submit">Авторизироваться</button>
-    </form>
-</body>
-</html>
+<?
+
+var_dump($_REQUEST);
